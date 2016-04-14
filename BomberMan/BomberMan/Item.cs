f6bd5a855c94d8d61319ca4691f875c75bb73bcc
Viktor Siljanovski <viktorsiljanovski21@gmail.com>
2016-04-14 12:37:11 +0200

    Created abstract class for Items

---
+++ BomberMan/BomberMan/Item.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BomberMan
+{
+
+    public abstract class Item
+    {
+        public string Name { get; set; }
+        public Point Position { get; set; }
+
+        public Item(string name)
+        {
+            Name = name;
+            Position = new Point();
+        }
+
+        public abstract void Draw();
+        public abstract void Action();
+    }
+}
