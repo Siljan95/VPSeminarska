6fcd55849c283e2a2c7aa60778b0e8e1b631f019
Viktor Siljanovski <viktorsiljanovski21@gmail.com>
2016-04-15 09:09:47 +0200

    Created the scene

---
+++ BomberMan/BomberMan/Scene.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BomberMan
+{
+    class Scene
+    {
+        public List<BomberMan> BomberMen { get; set; }
+
+        public Scene()
+        {
+            BomberMen = new List<BomberMan>();
+            
+        }
+
+        public void AddPlayer(BomberMan bomas)
+        {
+            BomberMen.Add(bomas);
+        }
+
+        public void Draw(Graphics g)
+        {
+            foreach(BomberMan b in BomberMen)
+            {
+                b.Draw(g);
+            }
+        }
+
+    }
+}
