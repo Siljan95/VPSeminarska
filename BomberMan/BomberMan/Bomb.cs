f3e3f94e2c68621088bd6f27d79d8d637fb2a609
Viktor Siljanovski <viktorsiljanovski21@gmail.com>
2016-04-17 15:35:20 +0200

    Fixed the bomb explosion

--- BomberMan/BomberMan/Bomb.cs
+++ BomberMan/BomberMan/Bomb.cs
@@ -4,51 +4,52 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Drawing;
+using System.Diagnostics;
 
 namespace BomberMan
 {
-  public  class Bomb 
+    public class Bomb 
     {
         public int BombRadius { get; set; }   
-      
         public Point Cordinates { get; set; }
-        public int Timer { get; set; }
-        public Brush Bomb_color { get; set; }
+        public int CountDown { get; set; }
+        public bool Exploded { get; set; }
 
         public Bomb(Point c) 
         {
             //Staveno e random kje treba da se smeni vo zavisnost od mapata
             BombRadius = 10;
-            Timer = 5;
-            Bomb_color = new SolidBrush(Color.Black);
+            CountDown = 3;
             Cordinates = c;
+            Exploded = false;
         }
 
         //Treba da se implementira countdown na bombata pred da eksplodira
-        public  void Explode( Graphics g)
+        public bool Explode(Graphics g)
         {
-            if (Timer != 0)
+            if (CountDown < 0)
             {
-                Timer -= 1;
+                Exploded = true;
             }
-            else
+            return Exploded;
+        }
+        
+
+        public void Draw(Graphics g)
+        {
+            if (Exploded)
             {
                 Pen pen = new Pen(Color.Red, 5);
                 g.DrawLine(pen, Cordinates.X - BombRadius, Cordinates.Y, Cordinates.X + BombRadius, Cordinates.Y);
                 g.DrawLine(pen, Cordinates.X, Cordinates.Y - BombRadius, Cordinates.X, Cordinates.Y + BombRadius);
-               
+                pen.Dispose();
             }
-        }
-        
-
-        public  void Draw(Graphics g)
-        {
-            Brush s = new SolidBrush(Color.Black);
-
-            g.FillEllipse(s, Cordinates.X, Cordinates.Y, 15, 15);
-
-         
-
+            else
+            {
+                Brush s = new SolidBrush(Color.Black);
+                g.FillEllipse(s, Cordinates.X, Cordinates.Y, 15, 15);
+                s.Dispose();
+            }
         }
     }
 }
